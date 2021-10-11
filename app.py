f07952c03f8a69409c82888b4727408bae12f1aa
Zoe Thexton <zoe.thexton@appeal.marketing>
2021-10-11 09:11:21 +0100

    Separate variables into global spec

--- app.py
+++ app.py
@@ -16,18 +16,51 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# Mongo collections
 cats_db = mongo.db.categories
 ings_db = mongo.db.ingredients
 recs_db = mongo.db.recipes
 units_db = mongo.db.units
 users_db = mongo.db.users
 
+# Mongo collections - list
 cats = list(mongo.db.categories.find())
 ings = list(mongo.db.ingredients.find())
 recs = list(mongo.db.recipes.find())
 units = list(mongo.db.units.find())
 
+# Current user (replace with session[user])
 user = users_db.find_one({"_id": ObjectId("60f19bbb944f8dacbba0b104")})
+
+# User cupboard
+user_cupboard = user['cupboard']
+user_cupboard_arr = []
+icupboard_max = len(user_cupboard)
+
+# User fav
+# user_fav = user['fav']
+# user_fav_arr = []
+# ifav_max = len(user_fav)
+
+# User house
+user_house = user['house']
+user_house_arr = []
+ihouse_max = len(user_house)
+
+# User menu
+# user_menu = user['menu']
+# user_menu_arr = []
+# imenu_max = len(user_menu)
+
+# User shopping
+# user_shopping = user['shopping']
+# user_shopping_arr = []
+# ishopping_max = len(user_shopping)
+
+# User spicerack
+user_spicerack = user['spicerack']
+user_spicerack_arr = []
+ispicerack_max = len(user_spicerack)
 
 
 @app.route("/")
@@ -47,14 +80,11 @@ def register():
 
 @app.route("/profile")
 def profile():
-    # Get ings in user's cupboard
-    user_cupboard = user['cupboard']
-    user_cupboard_arr = []
     i = 0
-    imax = len(user_cupboard)
 
+    # Get ings in user's cupboard
     if user_cupboard != []:
-        while i < imax:
+        while i < icupboard_max:
             user_cupboard_arr.append(ings_db.find_one({"_id": user_cupboard[i]['id']}))
             user_cupboard_arr[i]["bag"] = user_cupboard[i]['bag']
             i += 1
@@ -63,28 +93,22 @@ def profile():
     ings_cupboard = list(ings_db.find({"cat_name": ObjectId("615cb8323651f6c470f9a552")}))
 
     # Get spices in user's spicerack
-    user_spice = user['spicerack']
-    user_spice_arr = []
     i = 0
-    imax = len(user_spice)
 
-    if user_spice != []:
-        while i < imax:
-            user_spice_arr.append(ings_db.find_one({"_id": user_spice[i]['id']}))
-            user_spice_arr[i]["bag"] = user_spice[i]['bag']
+    if user_spicerack != []:
+        while i < ispicerack_max:
+            user_spicerack_arr.append(ings_db.find_one({"_id": user_spicerack[i]['id']}))
+            user_spicerack_arr[i]["bag"] = user_spicerack[i]['bag']
             i += 1
     
     # Get ings with spice category
     ings_spices = list(ings_db.find({"cat_name": ObjectId("615cb7b43651f6c470f9a551")}))
     
     # Get items in user's house
-    user_house = user['house']
-    user_house_arr = []
     i = 0
-    imax = len(user_house)
 
     if user_house != []:
-        while i < imax:
+        while i < ihouse_max:
             user_house_arr.append(ings_db.find_one({"_id": user_house[i]['id']}))
             user_house_arr[i]["bag"] = user_house[i]['bag']
             i += 1
@@ -95,7 +119,7 @@ def profile():
     return render_template("pages/profile/profile.html",
                            user = user,
                            user_cupboard = user_cupboard_arr,
-                           user_spices = user_spice_arr,
+                           user_spices = user_spicerack_arr,
                            user_house = user_house_arr,
                            ings = ings,
                            ings_cupboard = ings_cupboard,
@@ -116,7 +140,7 @@ def add_ingredient(select_name):
 
         # House
         if (ings_selected['cat_name'] == ObjectId("615cb8473651f6c470f9a553")):
-            print(ings_selected)
+            print((user['house'][0]['id']))
             users_db.update_one({"_id": ObjectId("60f19bbb944f8dacbba0b104")},
                                {'$push': {"house": {"id": ObjectId(ings_selected['_id']), "bag": True}}})
 
